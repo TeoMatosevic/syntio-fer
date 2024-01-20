7ed83dd7e490271165feea07f01829239d0f3c46
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2024-01-21 00:26:43 +0100

    Added lab1

---
+++ lab01/lab01_producer/lab01_producer.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from azure.eventhub import EventHubProducerClient, EventData
+
+connection_str = 'Endpoint=sb://tm-ehns-tpiuo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=BpXgI4gaS9suZ9+KaqzW8If9TGTaQwOEM+AEhIiQiWI='
+eventhub_name = 'tm-eh-tpiuo'
+url = "https://oauth.reddit.com/r/dataengineering/top.json?limit=10&t=all"
+redit_id = "Mpv44m4AX0UpydVDvowJDw"
+redit_secret = "w16rCZuCHXiLSBIUdgOTFfDLYIQOaQ"
+auth = requests.auth.HTTPBasicAuth(redit_id, redit_secret)
+data = {"grant_type": "password", "username": "Ancient_League_1716", "password": "ijhweubvceuirzv"}
+headers = {"User-agent": "windows:producer_app:v1.0 (by /u/Ancient_League_1716)"}
+
+token_response = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers)
+token = token_response.json()["access_token"]
+headers["Authorization"] = f"bearer {token}"
+
+response = requests.get(url, headers=headers)
+data = response.json()
+
+producer = EventHubProducerClient.from_connection_string(conn_str=connection_str,eventhub_name=eventhub_name)
+
+event_data_batch = producer.create_batch()
+for post in data['data']['children']:
+    event_data_batch.add(EventData(json.dumps(post['data'])))
+producer.send_batch(event_data_batch)
+
+while True:
+    pass
