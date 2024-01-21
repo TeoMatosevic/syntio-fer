45d9b91ae81ded2af5cffe3e5485b3cf4c69c0ca
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2024-01-21 16:24:24 +0100

    test workflow

--- lab01/lab01_consumer/lab01_consumer.py
+++ lab01/lab01_consumer/lab01_consumer.py
@@ -4,7 +4,6 @@ from azure.eventhub import EventHubConsumerClient
 
 # connection string information
 connection_str = 'Endpoint=sb://tm-ehns-tpiuo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=BpXgI4gaS9suZ9+KaqzW8If9TGTaQwOEM+AEhIiQiWI='
-# eventub name
 eventhub_name = 'tm-eh-tpiuo'
 consumer_group = "$Default"
 
