b45f4efaf23ef832c70355138b5d31fcfceb8817
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2024-01-21 16:35:26 +0100

    fixed lint

--- lab01/lab01_consumer/lab01_consumer.py
+++ lab01/lab01_consumer/lab01_consumer.py
@@ -3,9 +3,14 @@ import json
 from azure.eventhub import EventHubConsumerClient
 
 # connection string information
-connection_str = 'Endpoint=sb://tm-ehns-tpiuo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=BpXgI4gaS9suZ9+KaqzW8If9TGTaQwOEM+AEhIiQiWI='
-eventhub_name = 'tm-eh-tpiuo'
+connection_str = (
+    "Endpoint=sb://tm-ehns-tpiuo.servicebus.windows.net/;"
+    "SharedAccessKeyName=RootManageSharedAccessKey;"
+    "SharedAccessKey=BpXgI4gaS9suZ9+KaqzW8If9TGTaQwOEM+AEhIiQiWI="
+)
+eventhub_name = "tm-eh-tpiuo"
 consumer_group = "$Default"
+
 
 def on_event(partition_context, event):
     json_data = json.loads(event.body_as_str())
@@ -15,7 +20,9 @@ def on_event(partition_context, event):
     partition_context.update_checkpoint(event)
 
 
-consumer = EventHubConsumerClient.from_connection_string(connection_str, consumer_group=consumer_group, eventhub_name=eventhub_name)
+consumer = EventHubConsumerClient.from_connection_string(
+    connection_str, consumer_group=consumer_group, eventhub_name=eventhub_name
+)
 
 with consumer:
     consumer.receive(on_event=on_event)
